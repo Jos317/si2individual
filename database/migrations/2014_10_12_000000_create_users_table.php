0540a1d9478b54570952ed49659365a31e1e4a07
Jos317 <jchilelaime38@gmail.com>
2022-04-15 20:49:08 -0400

    estado

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('imagen')->default('');
+            $table->tinyint('estado')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
